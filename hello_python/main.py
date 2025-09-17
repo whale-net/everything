28cc66ea2ce0d7436efcfb8d2eaf7013e137cfff
qmastermoo <alex.harding@whale-net.net>
2025-09-16 23:37:02 -0400

    f

--- hello_python/main.py
+++ hello_python/main.py
@@ -10,7 +10,6 @@ def main():
     """Main entry point."""
     print(get_message())
     print(f"Version: {get_version()}")
-    print("that last commit was too early")
 
 if __name__ == "__main__":
     main()
